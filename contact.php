5aabc5fdd3983277544eaddcf765706819d640cf
jennifer <ortiztatiana1416@gmail.com>
2024-05-29 17:34:53 -0500

    Validación de contactar

--- contact.php
+++ contact.php
@@ -1,3 +1,54 @@
+
+<?php
+    session_start();
+    require_once("db/connection.php");
+    // include("../../../controller/validarSesion.php");
+    $db = new Database();
+    $con = $db -> conectar();
+
+
+   if ((isset($_POST["MM_insert"]))&&($_POST["MM_insert"]=="formreg"))
+   {
+    $nombre= $_POST['nombre'];
+    $apellido= $_POST['apellido'];
+    $nombre_comp = $nombre . " " . $apellido;
+    $cedula= $_POST['cedula'];
+    $celular= $_POST['celular'];
+    $contrasena= $_POST['contrasena'];
+    $correo= $_POST['correo'];
+    $tipo_user= 2;
+    $id_estado= 1;
+    $nit= 123456789; 
+
+     $sql= $con -> prepare ("SELECT * FROM usuarios WHERE cedula='$cedula'");
+     $sql -> execute();
+     $fila = $sql -> fetchAll(PDO::FETCH_ASSOC);
+
+     if ($fila){
+        echo '<script>alert ("DOCUMENTO YA EXISTE //CAMBIELO//");</script>';
+        echo '<script>window.location="index.php"</script>';
+     }
+
+     else
+   
+     if ($cedula=="" || $nombre=="" || $correo=="" || $celular=="" || $contrasena=="" || $tipo_user=="" || $id_estado=="" || $nit=="")
+      {
+         echo '<script>alert ("EXISTEN DATOS VACIOS");</script>';
+         echo '<script>window.location="index.php"</script>';
+      }
+      
+      else{
+
+        $pass_cifrado = password_hash($contrasena,PASSWORD_DEFAULT, array("pass"=>12));
+        
+        $insertSQL = $con->prepare("INSERT INTO usuarios(cedula, nombre, celular, contrasena, correo, id_tipo_user, id_estado, nit) VALUES('$cedula', '$nombre_comp', '$celular', '$pass_cifrado', '$correo', '$tipo_user', '$id_estado', '$nit')");
+        $insertSQL -> execute();
+        echo '<script> alert("REGISTRO EXITOSO");</script>';
+        echo '<script>window.location="index.php"</script>';
+     }  
+    }
+    ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -82,86 +133,79 @@
         <!-- Page Header End -->
 
 
-        <!-- Contact Start -->
-        <div class="container-xxl py-5">
+        <form method="post" name="formreg" id="formreg" class="signup-form"  autocomplete="off"> 
+        <!-- Appointment Start -->
+        <div class="container-xxl py-5" id="registrate">
             <div class="container">
-                <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
-                    <h1 class="mb-3">Contactanos</h1>
-                    <p>Estamos ubicados en La ciudad de Ibague, en el departamento del Tolima, sin embargo, realizamos evenos en todos los municipios del Tolima, ademas Girardot-Cundinamarca.</p>
-                </div>
-                <div class="row g-4 mb-5">
-                    <div class="col-md-6 col-lg-4 text-center wow fadeInUp" data-wow-delay="0.1s">
-                        <div class="bg-light rounded-circle d-inline-flex align-items-center justify-content-center mb-4" style="width: 75px; height: 75px;">
-                            <i class="fa fa-map-marker-alt fa-2x text-primary"></i>
-                        </div>
-                        <h6>Urbanizacion Anda Lucia Real, Barrio las Margaritas, Ibague-Tolima</h6>
-                    </div>
-                    <div class="col-md-6 col-lg-4 text-center wow fadeInUp" data-wow-delay="0.3s">
-                        <div class="bg-light rounded-circle d-inline-flex align-items-center justify-content-center mb-4" style="width: 75px; height: 75px;">
-                            <i class="fa fa-envelope-open fa-2x text-primary"></i>
-                        </div>
-                        <h6>Arlequin_Eventos@gmail.com</h6>
-                    </div>
-                    <div class="col-md-6 col-lg-4 text-center wow fadeInUp" data-wow-delay="0.5s">
-                        <div class="bg-light rounded-circle d-inline-flex align-items-center justify-content-center mb-4" style="width: 75px; height: 75px;">
-                            <i class="fa fa-phone-alt fa-2x text-primary"></i>
-                        </div>
-                        <h6>3157418168 - 3187808830</h6>
-                    </div>
-                </div>
                 <div class="bg-light rounded">
                     <div class="row g-0">
                         <div class="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
                             <div class="h-100 d-flex flex-column justify-content-center p-5">
-                                <p class="mb-4">Al llenar el siguiente formulario, estarias creando tu propio perfil en nuestro sitio web, por lo que si deseas reservar un evento, podras ingresar con tu usario y contraseña y reservarlo desde nuestro sitio web.</p>
-                                <form>
+                                <h1 class="mb-4">Regístrate</h1>
+                                <p>Regístrate para conocer más acerca de los paquetes y hacer tu reservación</p>
+                            
                                     <div class="row g-3">
                                         <div class="col-sm-6">
                                             <div class="form-floating">
-                                                <input type="text" class="form-control border-0" id="name" placeholder="Nombre Completo">
-                                                <label for="name">Nombre Completo</label>
+                                                
+                                                <input  class="form-control border-0" type="text" name="nombre" id="nombres" pattern="[a-zA-Z/s]{3,40}" title="Solo se permiten letras, minimo 3 digitos" placeholder="Digite Nombre" required>
+                                                <label for="gname">Nombres</label>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-sm-6">
+                                            <div class="form-floating">
+                                                <input class="form-control border-0" type="text" name="apellido" placeholder="Apellidos" pattern="[a-zA-Z/s]{3,40}" title="Solo se permiten letras, minimo 3 digitos" required>
+                                                <label for="fecha">Apellidos</label>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-sm-6">
+                                            <div class="form-floating">
+                                    
+                                                <input class="form-control border-0"  type="text" name="cedula" id="documento" pattern="[0-9]{7,15}" title="Solo se permiten numeros, minimo 7 digitos" placeholder="Digite Documento" required>
+                                                <label for="cedula">N° Documento</label>
                                             </div>
                                         </div>
                                         <div class="col-sm-6">
                                             <div class="form-floating">
-                                                <input type="number" class="form-control border-0" id="email" placeholder="Documento">
-                                                <label for="email">N° Documento</label>
+                                                
+                                                <input class="form-control border-0"   type="text" name="celular" id="telefono" pattern="[0-9]{7,15}" title="Solo se permiten numeros, minimo 10 digitos" placeholder="Digite Telefono" required>
+                                                <label for="cname">Contacto</label>
                                             </div>
                                         </div>
                                         <div class="col-sm-6">
                                             <div class="form-floating">
-                                                <input type="email" class="form-control border-0" id="email" placeholder="Correo">
-                                                <label for="email">Correo Electronico</label>
+                                                <input  class="form-control border-0"  type="email" name="correo" id="correo" placeholder="Digite Correo" required>
+                                                <label for="cage">Correo</label>
                                             </div>
                                         </div>
-                                        <div class="col-12">
+                                        <div class="col-sm-6">
                                             <div class="form-floating">
-                                                <input type="number" class="form-control border-0" id="email" placeholder="Telefono">
-                                                <label for="email">Telefono de Contacto</label>
+                                                <input class="form-control border-0" pattern="[a-zA-Z0-9]{8,15}" title="La contraseña debe tener minimo 8 digitos" type="password" name="contrasena" placeholder="Contraseña" required>
+                                                <label for="fecha">Contraseña</label>
                                             </div>
-                                        </div>
+                                        </div>                                        
+                                        
                                         <div class="col-12">
-                                            <div class="form-floating">
-                                                <textarea class="form-control border-0" placeholder="Leave a message here" id="message" style="height: 100px"></textarea>
-                                                <label for="message">Contraseña</label>
-                                            </div>
-                                        </div>
-                                        <div class="col-12">
-                                            <button class="btn btn-primary w-100 py-3" type="submit" name="registrar">Registrase</button>
+                                            
+                                            <input class="btn btn-primary w-100 py-3"   type="submit" name="registrarse" value="Registro">
+                                            <input   type="hidden" name="MM_insert" value="formreg">
                                         </div>
                                     </div>
-                                </form>
+                               
                             </div>
                         </div>
                         <div class="col-lg-6 wow fadeIn" data-wow-delay="0.5s" style="min-height: 400px;">
                             <div class="position-relative h-100">
-                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1988.9144383679056!2d-75.18760232341391!3d4.442929272459184!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e38c50fb2ddff45%3A0x59199eb4e87be4fd!2sUrbanizaci%C3%B3n%20Andalucia%20Real!5e0!3m2!1ses!2sco!4v1711054122223!5m2!1ses!2sco" width="550" height="610" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                                <img class="position-absolute w-100 h-100 rounded" src="imagenes/contenido/img19.jpg" style="object-fit: cover;">
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+ </form>
         <!-- Contact End -->
 
 
